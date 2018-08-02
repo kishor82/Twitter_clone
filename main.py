1ea46321649070b1e74a2bbd3edd5136221833e4
kishor <kishorrathva8298@gmail.com>
2018-08-02 20:39:19 +0530

    .

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template ,session ,request,redirect ,url_for ,flash
+from flask import Flask,render_template ,session ,request,redirect ,url_for ,flash ,g
 from flask_pymongo import PyMongo
-import bcrypt 
+from datetime import datetime
+from bson.objectid import ObjectId
+import bcrypt
 
 app=Flask(__name__)
 app.secret_key = 'mysecret'
@@ -12,8 +14,9 @@ mongo = PyMongo(app)
 def home():
 	if 'username' in session:
 		users = mongo.db.users
-		user_data = users.find_one({'_id':session['username']})
+		user_data = users.find_one({'name':session['username']})
 		#store all data as dictionary 
+		#all_user= users.find()
 		#records = dict((record['_id'],record) for record in all_user)
 		return render_template('home.html', session = session)
 
@@ -58,19 +61,49 @@ def register():
 	return render_template('register.html')
 
 
-@app.route('/profile')
-def profile():
-	return 'profile'
+@app.route('/profile/<id>')
+def profile(id):
+	users =mongo.db.users
+	user_data = users.find_one({'name':session['username']})
+	#it take about 4 hous to solve this very confusing though!!
+	profile_id=request.args.get('id')
+	profileData = users.find_one({'_id':ObjectId(profile_id)})
+
+	tweet =mongo.db.tweets
+
+	result = tweet.find()
+	records = dict((record['_id'],record) for record in result)
+
+	return render_template('home.html',records =records)
+
 
 @app.route('/userlist')
 def userlist():
-	return 'userlist'
+	return render_template('under_construction.html')
 
 @app.route('/logout')
 def logout():
-	return 'logout'
+	return render_template('under_construction.html')
+	 
+@app.route('/create_tweet',methods =['POST','GET'])
+def create_tweet():
+	if request.method == 'POST':
+		users = mongo.db.users
+		user_id = session['username']
+		user_data = users.find_one({'name':session['username']})
+		body = request.form['body']
+		date = datetime.now().strftime('%d-%m-%y %H:%M')
 
+		tweet =mongo.db.tweets
 
+		tweets = tweet.insert_one({
+			'authorID':user_id,
+			'authorName':user_data['name'],
+			'body':body,
+			'created':date
+			})
+
+		return redirect(url_for('home'))
 
 if __name__=='__main__':
 	app.run(debug =True)
